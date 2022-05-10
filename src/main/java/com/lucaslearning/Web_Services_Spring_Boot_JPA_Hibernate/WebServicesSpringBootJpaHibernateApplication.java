568a79e4e41d81b5ea003b66ea3801cda4c11ae0
Lucas Côrte <lucascorte@outlook.com>
2022-05-10 11:53:58 -0300

    Project created

---
+++ src/main/java/com/lucaslearning/Web_Services_Spring_Boot_JPA_Hibernate/WebServicesSpringBootJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.lucaslearning.Web_Services_Spring_Boot_JPA_Hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesSpringBootJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServicesSpringBootJpaHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucaslearning/Web_Services_Spring_Boot_JPA_Hibernate/WebServicesSpringBootJpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaslearning.Web_Services_Spring_Boot_JPA_Hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicesSpringBootJpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
