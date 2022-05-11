b8096f676ecde2f8f10de3095241af439b7c5ab0
Lucas Côrte <lucascorte@outlook.com>
2022-05-11 11:31:37 -0300

    H2 database, test profile, JPA

--- src/main/java/com/lucaslearning/Web_Services_Spring_Boot_JPA_Hibernate/entities/User.java
+++ src/main/java/com/lucaslearning/Web_Services_Spring_Boot_JPA_Hibernate/entities/User.java
@@ -3,9 +3,17 @@ package com.lucaslearning.Web_Services_Spring_Boot_JPA_Hibernate.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
