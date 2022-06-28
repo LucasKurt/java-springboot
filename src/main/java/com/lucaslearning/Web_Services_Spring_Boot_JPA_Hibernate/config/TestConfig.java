57a10464e63bbc22e6487d182db05dac401c304e
Lucas Côrte <lucascorte@outlook.com>
2022-06-27 22:36:16 -0300

    Payment, one-to-one association

--- src/main/java/com/lucaslearning/Web_Services_Spring_Boot_JPA_Hibernate/config/TestConfig.java
+++ src/main/java/com/lucaslearning/Web_Services_Spring_Boot_JPA_Hibernate/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.lucaslearning.Web_Services_Spring_Boot_JPA_Hibernate.entities.Category;
 import com.lucaslearning.Web_Services_Spring_Boot_JPA_Hibernate.entities.Order;
 import com.lucaslearning.Web_Services_Spring_Boot_JPA_Hibernate.entities.OrderItem;
+import com.lucaslearning.Web_Services_Spring_Boot_JPA_Hibernate.entities.Payment;
 import com.lucaslearning.Web_Services_Spring_Boot_JPA_Hibernate.entities.Product;
 import com.lucaslearning.Web_Services_Spring_Boot_JPA_Hibernate.entities.User;
 import com.lucaslearning.Web_Services_Spring_Boot_JPA_Hibernate.entities.enums.OrderStatus;
@@ -82,5 +83,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 	}
 }
