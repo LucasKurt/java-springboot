dcfa208fbbfb7749a377382213f46d7366089e70
Lucas Côrte <lucascorte@outlook.com>
2022-06-29 23:02:42 -0300

    Exception handling - delete

---
+++ src/main/java/com/lucaslearning/Web_Services_Spring_Boot_JPA_Hibernate/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.lucaslearning.Web_Services_Spring_Boot_JPA_Hibernate.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+}
